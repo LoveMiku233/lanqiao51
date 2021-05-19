d153c78a134c6c799cffe24df3d992151b777072
YaYazzz <83657064+YaYazzz@users.noreply.github.com>
2021-05-19 20:19:37 +0800

    Create Test2.java

---
+++ YaYa/java/Test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i=0;i<=100;i+=2){
+            sum = sum + i;
+        }
+        System.out.println("0到100之间所有偶数的之和为："+sum);
+    }
+}
